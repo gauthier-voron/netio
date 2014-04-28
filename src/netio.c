eb9e464fa330bfdbadea06fd6198695e4232400b
Gauthier Voron <gauthier.voron@mnesic.fr>
2014-04-28 20:53:43 +0200

    Add empty projet structure   - src/ is the root for all the sources   - include/ is the root for all the headers   - obj/ is the root for all the generated objects   - lib/ is where to store generated libraries   - bin/ is where to store generated binaries

---
+++ src/netio.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "netio.h"
+
+
+void dummy(void)
+{
+	printf("version = %s\n", NETIO_VERSION);
+}
---
+++ src/netprint.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "netio.h"
+
+
+int main(void)
+{
+	dummy();
+	return EXIT_SUCCESS;
+}
