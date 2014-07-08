42977a07f0279e7c09f08ebb2ed017cd22c47a62
Gauthier Voron <gauthier.voron@mnesic.fr>
2014-07-08 21:39:13 +0200

    feat(udp): add the header
    Add the header for UDP protocol with the four fields defined
    in RFC 768.

---
+++ include/netio/udp.h
@@ -0,0 +1,50 @@
+#ifndef NETIO_UDP_H
+#define NETIO_UDP_H
+
+
+struct netio_udp;
+typedef struct netio_udp netio_udp_t;
+
+
+#include "netio/header.h"
+#include "netio/protocol.h"
+
+
+extern netio_protocol_t *NETIO_UDP_PROTOCOL;
+
+
+struct netio_udp
+{
+	netio_header_t   nudp_header;
+	int              nudp_src;
+	int              nudp_dest;
+	int              nudp_len;
+	int              nudp_sum;
+};
+
+
+int netio_udp_init(netio_udp_t *this);
+
+
+int netio_udp_checksum(const netio_udp_t *this);
+
+
+int netio_udp_setsrc(netio_udp_t *this, int src);
+
+int netio_udp_setdest(netio_udp_t *this, int dest);
+
+int netio_udp_setlen(netio_udp_t *this, int len);
+
+int netio_udp_setsum(netio_udp_t *this, int sum);
+
+
+int netio_udp_getsrc(const netio_udp_t *this);
+
+int netio_udp_getdest(const netio_udp_t *this);
+
+int netio_udp_getlen(const netio_udp_t *this);
+
+int netio_udp_getsum(const netio_udp_t *this);
+
+
+#endif
